bb5c9466f2c872bafe485a763e6581510308c2ae
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 01:52:14 -0300

    operators

---
+++ src/operators/Operators.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int sum1 = 1 + 2;
+        int difference = 2 - 1;
+        int multiplication = 1 * 2;
+        int division = 2 / 2;
+        int modulus = 10 % 2;
+        int increment = difference++;
+        int decrement = sum1--;
+    }
+}
