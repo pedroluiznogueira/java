d44b1c2f39c51b17ab9297ff79aed2f2c5788363
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 02:19:12 -0300

    parameters

---
+++ src/methods/parameters/Paramters.java
@@ -0,0 +1,22 @@
+package methods.parameters;
+
+public class Paramters {
+
+    // Create a checkAge() method with an integer variable called age
+    static void checkAge(int age) {
+
+        // If age is less than 18, print "access denied"
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+
+            // If age is greater than, or equal to, 18, print "access granted"
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        checkAge(20); // Call the checkAge method and pass along an age of 20
+    }
+}
