8bf3b512cdb2a9c5697bce7fab947ca1d50730fd
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 02:45:19 -0300

    constructors

---
+++ src/oop/constructors/Constructors.java
@@ -0,0 +1,21 @@
+package oop.constructors;
+
+public class Constructors {
+    public int x;
+
+    public Constructors() {
+        this.x = 20;
+    }
+
+    public Constructors(int x) {
+        this.x = x;
+    }
+
+    public static void main(String[] args) {
+        Constructors object = new Constructors();
+        System.out.println(object.x);
+
+        Constructors anotherObject = new Constructors(50);
+        System.out.println(anotherObject.x);
+    }
+}
