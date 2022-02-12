01232dfa228bf1d2a7b95dbb3066bb29126fbeef
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 01:49:57 -0300

    type casting

---
+++ src/type/casting/TypeCasting.java
@@ -0,0 +1,15 @@
+package type.casting;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        // automatic casting
+        int myInt = 9;
+        double myDouble = myInt;
+
+        // manual casting
+        double someDouble = 9.78d;
+        int someInt = (int) someDouble;
+    }
+}
