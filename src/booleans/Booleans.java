e673670c9125ab29c6811731b2af8ac6d1ce928a
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 02:00:04 -0300

    booleans

---
+++ src/booleans/Booleans.java
@@ -0,0 +1,12 @@
+package booleans;
+
+public class Booleans {
+
+    public static void main(String[] args) {
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+    }
+}
