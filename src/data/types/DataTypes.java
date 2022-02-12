5e816b9d65ebf2a47ae68afcd6f1b686521f4bc8
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 01:46:49 -0300

    data types

---
+++ src/data/types/DataTypes.java
@@ -0,0 +1,18 @@
+package data.types;
+
+public class DataTypes {
+    int myNum = 5;               // Integer (whole number)
+    float myFloatNum = 5.99f;    // Floating point number
+    char myLetter = 'D';         // Character
+    boolean myBool = true;       // Boolean
+    String myText = "Hello";     // String
+
+    // Integer types
+
+    byte myNumByte = 100;
+    short myNumShort = 5000;
+    int myNumInt = 100000;
+    long myNumLong = 15000000000L;
+    float myNumFloat = 5.75f;
+    double myNumDouble = 19.99d;
+}
