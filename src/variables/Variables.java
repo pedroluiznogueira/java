0e3b45bd77ec229cf2d50bfd9eeae463f580ca1c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-02-12 01:43:16 -0300

    variables

---
+++ src/variables/Variables.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        int z = 5, h = 6, i = 50;
+        System.out.println(z + h + i);
+    }
+}
